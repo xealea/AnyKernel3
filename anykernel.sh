3531f0f8cb991b8d2aa2055659f7df674b0bfdfa
ʟᴇᴀ <vcyzteen@protonmail.com>
2022-08-25 12:37:36 +0700

    xea: alioth/in

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Stable Kernel
+kernel.string=xea-xo4 Kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=X00T
-device.name2=X00TD
-device.name3=x00t
-device.name4=x00td
+device.name1=alioth
+device.name2=aliothin
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/soc/c0c4000.sdhci/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
